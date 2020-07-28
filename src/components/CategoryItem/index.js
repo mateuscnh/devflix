03237bee453cf6aa8d467fadf49eeb4b19d431ae
Mateus Cunha <mtscnh2@gmail.com>
2020-07-28 02:01:40 -0300

    fix(CategoryItem): link redirect.

--- src/components/CategoryItem/index.js
+++ src/components/CategoryItem/index.js
@@ -4,12 +4,14 @@ import { Container, Description } from './styles';
 
 export default function CategoryItem({ data }) {
     return (
-        <Container>
-            <Description>
-                <h1>{data.title}</h1>
-                <p>Player</p>
-            </Description>
-            <a href={data.url} target="_blanck"><img src={data.imageURL} alt="" /></a>
-        </Container >
+        <a href={data.url} target="_blanck">
+            <Container>
+                <Description>
+                    <h1>{data.title}</h1>
+                    <p>Player</p>
+                </Description>
+                <img src={data.imageURL} alt="" />
+            </Container >
+        </a>
     );
 }
