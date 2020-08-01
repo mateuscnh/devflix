4908c96d94d02445cf784ecaad5f25589b59fd64
Mateus Cunha <mtscnh2@gmail.com>
2020-08-01 00:44:43 -0300

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaLinkedinIn, FaInstagram, FaFacebookF, FaGithub } from 'react-icons/fa';
 
 import Logo from '../../assets/logo.png';
 
@@ -10,8 +11,23 @@ const Container = styled.footer`
     margin-top: 50px;
 
     img{
-        margin-bottom: 10px;
-        height: 30px;
+        margin-bottom: 6px;
+        height: 24px;
+    }
+    p{
+        font-size: 14px;
+    }
+
+    .social{
+        margin-top: 4px;
+        a{
+            margin: 10px;
+            transition: .2s;
+            :hover{
+                opacity: .5;
+            }
+        }
+
     }
 `;
 
@@ -19,6 +35,12 @@ export default function Footer() {
     return (
         <Container>
             <img src={Logo} alt="" />
+            <div className="social">
+                <a href="https://github.com/mateuscnh" rel="noopener noreferrer" target="_blank"><FaGithub /></a>
+                <a href="https://www.linkedin.com/in/mateus-cunha-a886141a5/" rel="noopener noreferrer" target="_blank"><FaLinkedinIn /></a>
+                <a href="https://www.instagram.com/mateuscnh/" rel="noopener noreferrer" target="_blank"><FaInstagram /></a>
+                <a href="https://www.facebook.com/MateusCnh/" rel="noopener noreferrer" target="_blank"><FaFacebookF /></a>
+            </div>
             <p>© 2020, @mateuscnh. All Rights Reserved.</p>
         </Container>
     );
